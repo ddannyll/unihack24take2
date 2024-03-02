5feede1ee4c7fd41fff9b7741266f512413abb99
Neosh <neosh11@gmail.com>
2024-03-03 01:50:57 +1100

    fuck this

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />;
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <Component {...pageProps} />;
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
--- web/src/pages/matchroom.tsx
+++ web/src/pages/matchroom.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import PocketBase from "pocketbase";
+
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export interface PeepResponse {
   id: string;
@@ -12,20 +17,97 @@ export interface PeepResponse {
   "@expand": {};
 }
 
-// export const client = new PocketBase("http://192.168.43.238:8090");
-export const client = new PocketBase("http://127.0.0.1:8090");
-export const realTime = async (index: [string], queryClient: QueryClient) => {
-  return await client.realtime.subscribe("peeps", function (e) {
-    console.log("real time peeps", e.record);
-  });
-};
+const HARD_CODED = "fbc9i9k93o0mlxd";
 
-export const allPeeps = async (): Promise<PeepResponse[] | Record[]> => {
-  return await client.records.getFullList("peeps", 200 /* batch size */, {
-    sort: "-created",
-  });
+// export const client = new PocketBase("http://192.168.43.238:8090");
+const pb = new PocketBase("http://127.0.0.1:8090");
+
+interface Message {
+  id: string;
+  text: string;
+  user?: string;
+}
+
+export const appendToCache = async (
+  index: [string],
+  queryClient: QueryClient,
+  newData: any
+) => {
+  queryClient.setQueryData(index, newData);
 };
 
 export default function MatchRoom() {
-  return <div>hello world</div>;
+  const queryClient = new QueryClient();
+
+  const { data: todo, isLoading } = useQuery({
+    queryKey: ["peeps"],
+    queryFn: async () => {
+      const res = await pb.collection("peeps").getOne(HARD_CODED);
+      return res;
+    },
+  });
+
+  const { data: messages, isLoading: messagesIsLoading } = useQuery({
+    queryKey: ["messages"],
+    queryFn: async () => {
+      const res = (await pb.collection("messages").getFullList()).map(
+        (rec): Message => {
+          return {
+            id: rec.id,
+            text: rec.data.text,
+            user: rec.data.user,
+          };
+        }
+      );
+      return res;
+    },
+  });
+
+  useEffect(() => {
+    // try to create an object with the same id as the one we are fetching if it doesn't exist
+
+    // fetch the object once and then subscribe to it
+    const unsubscribePromise = pb
+      .collection("peeps")
+      .subscribe(HARD_CODED, (e) => {
+        appendToCache(["peeps"], queryClient, e.record);
+      });
+
+    const unsubscribePromise2 = pb
+      .collection("messages")
+      .subscribe("*", (e) => {
+        console.log("wr2", e);
+
+        if (e.action === "create") {
+          appendToCache(["messages"], queryClient, e.record);
+        }
+      });
+
+    return () => {
+      (async () => {
+        (await unsubscribePromise)();
+      })();
+    };
+  }, []);
+
+  console.log("data", todo);
+  console.log("messages", messages);
+
+  return (
+    <>
+      <div>{JSON.stringify(todo)}</div>
+
+      <hr />
+
+      {/* map the messages */}
+
+      {messages?.map((message: { id: string; text: string }) => {
+        return (
+          <div key={message.id}>
+            <div>{message.text}</div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
